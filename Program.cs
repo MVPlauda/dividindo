43d8f3e3f5b7abc4b5d89540122281271e6249e6
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:44:04 -0300

    dividindo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        float num1, num2, divisão;
+        
+        Console.Write("Digite um numero: ");
+        num1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        num2 = float.Parse(Console.ReadLine());
+
+        divisão = num1 / num2;
+
+        Console.Write($"A divisão dos 2 numeros é: {divisão}");
+
+
+    }
+}
+
